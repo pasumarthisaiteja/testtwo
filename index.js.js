f5472d0068132e285590db7805f0a431a3974fcf
pasumarthisaiteja <61423320+pasumarthisaiteja@users.noreply.github.com>
2023-05-30 15:06:24 +0530

    test

---
+++ index.js.js
@@ -0,0 +1,8 @@
+function myfunction()   
+ {   
+    let a,b;
+    a=document.getElementById("fname").value;
+    b=document.getElementById("lname").value;
+    alert("Hey "+a+b+" How are you");
+
+ }  
